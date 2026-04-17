337e2579599bcdc1d61de4acf76a759e2f6a0652
sarellasanastasios <sarellasanastasios@hotmail.com>
2026-04-17 19:04:31 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.end('Hello from Docker + GitHub Actions + ACR!');
+});
+
+server.listen(8080, () => {
+  console.log('Server running on port 8080');
+});
 
