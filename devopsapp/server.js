1287cc03826da573107d7941b9fd875ce11ac22d
sarellasanastasios <sarellasanastasios@hotmail.com>
2026-04-24 11:18:49 +0300

    Update server.js

--- devopsapp/server.js
+++ devopsapp/server.js
@@ -1,1 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 8080;
 
+app.get('/', (req, res) => {
+  res.send('Hello from DevOps App running on AKS!');
+});
+
+app.listen(port, () => {
+  console.log('Server running on port ${port}');
+});  
