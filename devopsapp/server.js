cdf3a57170b7a35c35e12c9044f172e8417a2255
sarellasanastasios <sarellasanastasios@hotmail.com>
2026-04-27 11:41:18 +0300

    Update server.js

--- devopsapp/server.js
+++ devopsapp/server.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const app = express();
-const port = 8080;
+
+// Use PORT from environment (Kubernetes), fallback to 8080
+const port = process.env.PORT || 8080;
 
 app.get('/', (req, res) => {
   res.send('Hello from DevOps App running on AKS!');
 });
 
 app.listen(port, () => {
-  console.log('Server running on port ${port}');
-});  
+  console.log(`Server running on port ${port}`);
+});
+
